401c03918f365240230c3bfad935b365448aa3fc
Angeline Chew <chewmunyoke@gmail.com>
2024-08-28 16:45:11 +0800

    feat: reverse geocoding (city often not found)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,45 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
+import { apiURLReverseGeocoding } from '@/constants';
+import { GeocodingResponse } from '@/types';
 import { getLocationPath } from '@/utils/helpers';
-import { getLatestLocation } from '@/utils/storage';
+import { addLocation, getLatestLocation } from '@/utils/storage';
 
 export default function HomePage() {
   const router = useRouter();
 
-  useEffect(() => {
+  const fallback = () => {
     const location = getLatestLocation();
     router.push(getLocationPath(location));
+  };
+
+  const fetchReverseGeocoding = async (lat: number, lon: number) => {
+    const res = await fetch(apiURLReverseGeocoding(lat, lon));
+    const data = await res.json();
+    if (res.ok) {
+      const successData = data as GeocodingResponse[];
+      if (data.length) {
+        const location = `${successData[0].name}, ${successData[0].country}`;
+        addLocation(location);
+        router.push(getLocationPath(location));
+      } else {
+        fallback();
+      }
+    } else {
+      fallback();
+    }
+  };
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported by this browser');
+    } else {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        fetchReverseGeocoding(latitude, longitude);
+      }, fallback);
+    }
   }, []);
 
   return (
--- src/constants.ts
+++ src/constants.ts
@@ -25,6 +25,14 @@ export const apiURLGeocoding = (query: string): string =>
   `https://api.openweathermap.org/geo/1.0/direct?appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&limit=1&q=${query}`;
 
 /**
+ * API Reference: https://openweathermap.org/api/geocoding-api
+ *
+ * `limit` is `1` by default
+ */
+export const apiURLReverseGeocoding = (lat: number, lon: number): string =>
+  `https://api.openweathermap.org/geo/1.0/reverse?appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&limit=1&lat=${lat}&lon=${lon}`;
+
+/**
  * API Reference: https://openweathermap.org/weather-conditions
  */
 export const iconURL = (id: string): string =>
